0dd1e60dc55dd22eeda8662545f984af345501a1
cha <chyb627@naver.com>
2023-06-04 21:20:04 +0900

    예외처리 custom 해보기 ( Exception filter)

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, HttpException, Patch, Post, Put, UseFilters } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Controller('cats')
-export class CatsController {}
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  // @UseFilters(HttpExceptionFilter)
+  getAllCat() {
+    // throw new HttpException({ success: false, message: 'api is broken' }, 401);
+    throw new HttpException('api is broken', 401);
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return;
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
