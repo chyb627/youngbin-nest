6144e793dc3b0b394f696552bfe8ba5407711ae7
cha <chyb627@naver.com>
2023-06-11 23:06:09 +0900

    자신이 생성한 게시물 삭제하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -42,8 +42,8 @@ export class BoardsController {
   }
 
   @Delete('/:id')
-  deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
-    return this.boardsService.deleteBoard(id);
+  deleteBoard(@Param('id', ParseIntPipe) id, @GetUser() user: User): Promise<void> {
+    return this.boardsService.deleteBoard(id, user);
   }
 
   @Patch('/:id/status')
