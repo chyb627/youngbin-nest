2ea146407a32fe72c2e50c262b786e0907024bb6
cha <chyb627@naver.com>
2023-09-11 15:44:34 +0900

    ConfigModule 환경설정 적용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ip } from './decorators/ip.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly configService: ConfigService) {}
 
   private readonly logger = new Logger();
 
   @Get()
   getHello(@Ip() ip: string): string {
     this.logger.log(ip);
+    console.log(this.configService.get<string>('ENVIRONMENT'));
     return this.appService.getHello();
     // throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
 import { LoggingMiddleware } from './middleware/logging.middleware';
+import ConfigModule from './config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [ConfigModule(), BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
