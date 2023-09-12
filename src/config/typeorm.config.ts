b2a4bdfd26e49a3d8d3885a31eb9483874788ea0
cha <chyb627@naver.com>
2023-09-12 22:57:00 +0900

    오류 수정 : No metadata for "Board" was found.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import { Board } from 'src/entity/board.entity';
+import { User } from 'src/entity/user.entity';
 
 config({ path: '.env.local' });
 
@@ -10,6 +12,6 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [__dirname + '/**/*.entity{.ts,.js}', Board, User],
   synchronize: false,
 };
