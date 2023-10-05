754d4bb85d85d9c65dd8e83886118619cb50789f
cha <chyb627@naver.com>
2023-10-05 13:52:47 +0900

    bcrypt로 비밀번호 hash화

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { UserService } from 'src/user/user.service';
 import { RefreshToken } from './entity/refresh-token.entity';
 import { DataSource, Repository } from 'typeorm';
 import { User } from 'src/user/entity/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -24,7 +25,10 @@ export class AuthService {
     try {
       const user = await this.userService.findOneByEmail(email);
       if (user) throw new BadRequestException('Email is already existed');
-      const userEntity = queryRunner.manager.create(User, { email, password });
+
+      const saltRounds = 10;
+      const hash = await bcrypt.hash(password, saltRounds);
+      const userEntity = queryRunner.manager.create(User, { email, password: hash });
       await queryRunner.manager.save(userEntity);
       // throw new Error('새로운 USER를 DB에 저장하고 서버상에 에러가 나는경우');
       // 위와 같이 에러가 발생하면 트랜잭션을 명시적으로 사용하지 않고 default로 사용하면 DB는 저장이되지만 refresh-token은 발생하지 않게 된다.
@@ -48,11 +52,7 @@ export class AuthService {
   }
 
   async signin(email: string, password: string) {
-    const user = await this.userService.findOneByEmail(email);
-    if (!user) throw new UnauthorizedException();
-
-    const isMatch = password === user.password;
-    if (!isMatch) throw new UnauthorizedException();
+    const user = await this.validateUser(email, password);
 
     const refreshToken = await this.generateRefreshToken(user.id);
     await this.createRefreshTokenUsingUser(user.id, refreshToken);
@@ -94,5 +94,15 @@ export class AuthService {
     }
 
     await this.refreshTokenRepository.save(refreshTokenEntity);
+  }
+
+  private async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.userService.findOneByEmail(email);
+    if (!user) throw new UnauthorizedException();
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new UnauthorizedException();
+
+    return user;
   }
 }
